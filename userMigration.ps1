782fb5518e002af8972fbca14167c745b315c716
Svilen Grandzhan <new.tube.student@gmail.com>
2024-08-07 21:05:39 -0700

    final 20240807

--- userMigration.ps1
+++ userMigration.ps1
@@ -1,5 +1,10 @@
 $path = ".\list.csv"
+# File structure inside csv file
+#firstName,lastName,userObjectId,UPN
+#john,doe,1a2b-3c4d-5e6f,john.doe@domain.com
+# etc.
+
 
 Import-Csv $path | Foreach-Object { 
-    Write-Host $_.UPN 
+    Update-MgUser -UserId "$($_.userObjectId)" -UserPrincipalName "$($_.UPN)"
 }
